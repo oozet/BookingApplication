41df91dbf55e22d5c64302282be2bdf684ec7912
DefinitelyUnlikely <martin@kaarjohansson.se>
2025-05-06 16:47:18 +0200

    Add: RoomEntity model

--- Backend/Models/ActivityEntity.cs
+++ Backend/Models/ActivityEntity.cs
@@ -1,1 +1,24 @@
 namespace BookingApplication.Models;
+
+public class RoomEntity
+{
+    public string Id { get; set; } = null!;
+    public string Name { get; set; } = null!;
+    public int RoomNumber { get; set; }
+    public int Limit { get; set; }
+    public int Area { get; set; }
+    public double Price { get; set; }
+
+    public RoomEntity(string name, int roomNumber, int limit, int area, double price)
+    {
+        Id = Guid.NewGuid().ToString();
+        Name = name;
+        RoomNumber = roomNumber;
+        Limit = limit;
+        Area = area;
+        Price = price;
+    }
+
+    private RoomEntity() { }
+
+}
