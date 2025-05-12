2be5709c0ee7a818a63b151765904d3975c575c4
oozet <oozet@users.noreply.github.com>
2025-05-12 21:53:25 +0200

    fix: forgot to save

--- Backend/Interfaces/IService.cs
+++ Backend/Interfaces/IService.cs
@@ -1,5 +1,6 @@
 namespace BookingApplication.Interfaces;
 
+// Option 1
 public interface IService<T, TAddRequest, TEditRequest>
     where T : class
     where TAddRequest : IRequest
@@ -14,4 +15,20 @@ public interface IService<T, TAddRequest, TEditRequest>
     public Task DeleteAsync(T entityToRemove);
     public Task<T?> DeleteAsync<TKey>(TKey entityId)
         where TKey : notnull;
+}
+
+// Option 2, must declare TKey when inheriting
+public interface IService<T, TAddRequest, TEditRequest, TKey>
+    where T : class
+    where TAddRequest : IRequest
+    where TEditRequest : IRequest
+    where TKey : notnull
+{
+    public Task<T> CreateFromRequestAsync(TAddRequest request);
+    public Task<T?> GetByIdAsync(TKey entityId);
+    public Task<IEnumerable<T>> GetAllAsync();
+    public Task EditAsync(T entityToEdit);
+    public Task<T> EditFromRequestAsync(TEditRequest request);
+    public Task DeleteAsync(T entityToRemove);
+    public Task<T?> DeleteAsync(TKey entityId);
 }
