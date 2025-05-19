a43b081fb7e6b976189a7c02aaf6d2dc0edc10b8
DefinitelyUnlikely <martin@kaarjohansson.se>
2025-05-19 10:43:05 +0200

    Add an about page to showcase routing for group members, add a layout to show how that works + a navbar that can be rendered by the layout

---
+++ frontend/src/components/navbar.svelte
@@ -0,0 +1,72 @@
+<script lang="ts">
+</script>
+
+<nav class="nav-bar">
+	<div class="topnav">
+		<h1 class="title">Booking App</h1>
+	</div>
+	<div class="bottomnav">
+		<div class="bottom-left"><a href="/">Home</a><a href="/about">About</a></div>
+		<div class="bottom-middle">Middle</div>
+		<div class="bottom-right">Right</div>
+	</div>
+</nav>
+
+<style>
+	.nav-bar {
+		background-color: black;
+		width: 100%;
+		height: 6rem;
+		margin: 0;
+	}
+
+	.nav-bar {
+		color: white;
+	}
+
+	.topnav {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.title {
+		color: white;
+	}
+
+	.bottomnav {
+		display: grid;
+		grid-template-columns: 2fr 8fr 2fr;
+	}
+
+	.bottom-left {
+		display: flex;
+		gap: 1rem;
+		justify-content: flex-start;
+		align-items: center;
+		margin-left: 1rem;
+	}
+
+	.bottom-middle {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.bottom-right {
+		display: flex;
+		justify-content: flex-end;
+		align-items: center;
+		margin-right: 1rem;
+	}
+
+	nav a,
+	nav a:visited {
+		color: white;
+		text-decoration: none;
+	}
+
+	nav a:hover {
+		color: orange;
+	}
+</style>
---
+++ frontend/src/routes/about/+page.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	let creators: string[] = $state(['Anthon', 'Cedric', 'Martin', 'Olof']);
+
+	let visible: boolean = $state(false);
+
+	function changeVisibility() {
+		visible = !visible;
+	}
+</script>
+
+<p>
+	Detta är just nu en route jag använder för att visa hur 1. Routing funkar i svelte. 2. Vissa
+	forloop och if satser.
+</p>
+
+<button onclick={changeVisibility}>Vilka är med i gruppen?</button>
+
+{#if visible}
+	<ul>
+		{#each creators as creator}
+			<li>{creator}</li>
+		{/each}
+	</ul>
+{/if}
+
+<style>
+</style>
