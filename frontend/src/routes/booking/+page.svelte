284b0165fc40ae16531f405015d91a419302be6f
oozet <oozet@users.noreply.github.com>
2025-05-19 11:44:27 +0200

    feat: Tried adding login and booking page. Not sure if it works.

---
+++ frontend/src/routes/booking/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import { authStore } from '../../stores/authStore';
+    let user: any;
+    $: authStore.subscribe(value => user = value);
+</script>
+
+{#if !user}
+    <p>You must be logged in to view this page.</p>
+    <p><a href="/login">Login</a></p>
+{:else}
+    <p>Welcome, {user.username}!</p>
+{/if}
