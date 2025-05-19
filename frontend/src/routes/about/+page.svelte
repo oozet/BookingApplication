282771d87d1a4e0d3bc9452bf7dd5f6dc64f4c69
DefinitelyUnlikely <martin@kaarjohansson.se>
2025-05-19 10:47:46 +0200

    Add some more text about how routing works

--- frontend/src/routes/about/+page.svelte
+++ frontend/src/routes/about/+page.svelte
@@ -13,6 +13,12 @@
 	forloop och if satser.
 </p>
 
+<p>
+	Routing funkar alltså så enkelt att vi skapar en mapp under routes mappen, där namnet är routen vi
+	vill använda. T.ex. att jag nu gjort en /about route genom att skapa en mapp med namnet about. I
+	den lägger man en fil vid namn +page.svelte. Den är det som renderas när man är på den routen.
+</p>
+
 <button onclick={changeVisibility}>Vilka är med i gruppen?</button>
 
 {#if visible}
