3fb98121af5441b45b093b7e7abd5f5efd01dabb
DefinitelyUnlikely <martin@kaarjohansson.se>
2025-05-19 21:45:52 +0200

    remove zombie code

--- Backend/Controllers/BookingController.cs
+++ Backend/Controllers/BookingController.cs
@@ -87,22 +87,3 @@ public class BookingController(IService<Booking, CreateBookingRequest, EditBooki
     }
 }
 
-// public class CreateBookingRequest : IRequest
-// {
-//     public required DateTime StartDate { get; set; }
-//     public required DateTime EndDate { get; set; }
-//     public required Guid RoomId { get; set; }
-//     public required string UserId { get; set; }
-//     public Guid? ActivityId { get; set; }
-// }
-
-// public class EditBookingRequest : IRequest
-// {
-//     public required Guid Id { get; set; }
-//     public required DateTime StartDate { get; set; }
-//     public required DateTime EndDate { get; set; }
-//     public required Guid RoomId { get; set; }
-//     public required string UserId { get; set; }
-//     public Guid? ActivityId { get; set; }
-// }
-
