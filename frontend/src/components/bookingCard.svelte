1a0d6b71fd1a9fb8fb701cf55cbd4a692b239d57
DefinitelyUnlikely <martin@kaarjohansson.se>
2025-05-27 09:44:51 +0200

    add the fake mock objects as props to send to the card for testing

--- frontend/src/components/bookingCard.svelte
+++ frontend/src/components/bookingCard.svelte
@@ -1,38 +1,14 @@
 <script lang="ts">
 	// We'll fetch this data in th slug page server and then send it
 	// as a prop to this component to make the component be more of a component.
-	let booking = {
-		Id: 'mock Id for booking',
-		RoomId: 'mock id for the room',
-		UserId: 'mock id for the user',
-		ActivityId: 'Mock id for the activity',
-		StartDate: 'Add a date here',
-		EndDate: 'Add a date here as well'
-	};
-
-	let user = {
-		Id: 'mock id for the user',
-		Name: 'Mock Mockson'
-	};
-
-	let room = {
-		Id: 'mock id for the room',
-		Number: 404,
-		Name: 'Made up'
-	};
-
-	let activity = {
-		Id: 'Mock id for the activity',
-		Name: 'Activity Name',
-		Description: 'Activity description'
-	};
+	let { booking = {}, user = {}, room = {}, activity = {} } = $props();
 </script>
 
 <div class="booking-card">
 	<h2>Booking</h2>
-	<p><strong>Room#: </strong>{room.Number}</p>
-	<p><strong>Activity: </strong>{activity.Name}</p>
-	<p><strong>Booker: </strong>{user.Name}</p>
+	<p><strong>Room#:</strong> {room.Number}</p>
+	<p><strong>Activity:</strong> {activity.Name}</p>
+	<p><strong>Booker:</strong> {user.Name}</p>
 </div>
 
 <style>
--- frontend/src/routes/booking/[slug]/+page.svelte
+++ frontend/src/routes/booking/[slug]/+page.svelte
@@ -1,5 +1,36 @@
 <script lang="ts">
+	import BookingCard from '../../../components/bookingCard.svelte';
+
 	let { data } = $props();
+
+	// some mock data to send as a prop, for testing.
+	let booking = {
+		Id: 'mock Id for booking',
+		RoomId: 'mock id for the room',
+		UserId: 'mock id for the user',
+		ActivityId: 'Mock id for the activity',
+		StartDate: 'Add a date here',
+		EndDate: 'Add a date here as well'
+	};
+
+	let user = {
+		Id: 'mock id for the user',
+		Name: 'Mock Mockson'
+	};
+
+	let room = {
+		Id: 'mock id for the room',
+		Number: 404,
+		Name: 'Made up'
+	};
+
+	let activity = {
+		Id: 'Mock id for the activity',
+		Name: 'Activity Name',
+		Description: 'Activity description'
+	};
 </script>
 
 Hello from booking/{data.slug}
+
+<BookingCard {booking} {user} {room} {activity} />
