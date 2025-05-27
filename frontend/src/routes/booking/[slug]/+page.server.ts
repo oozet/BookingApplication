e664e68da619289ce17913bbf503c2e789e75c64
DefinitelyUnlikely <martin@kaarjohansson.se>
2025-05-28 00:43:53 +0200

    feat: add pageserver fetching

--- frontend/src/routes/booking/[slug]/+page.server.ts
+++ frontend/src/routes/booking/[slug]/+page.server.ts
@@ -2,9 +2,31 @@
 // then in the page, we'll use the bookingcard component and send the data we fetch here
 // as a prop.
 import type { PageServerLoad } from './$types';
+import { error } from '@sveltejs/kit';
+import { API_BASE_URL } from '$lib/constants';
+
+
 
 export const load: PageServerLoad = async ({ params }) => {
+    let booking = await fetch(API_BASE_URL + "/booking/" + params.slug);
+
+    if (!booking.ok) {
+        error(404, 'Booking not found');
+    }
+
+    let bookBody = await booking.json();
+    let room = await fetch(API_BASE_URL + "/room/" + bookBody.RoomId);
+
+    if (!room.ok) {
+        error(404, 'Room not found');
+    }
+    let roomBody = await room.json();
+    // let user = await fetch();
+
     return {
-        slug: params.slug
-    };
+        slug: params.slug,
+        booking: bookBody,
+        room: roomBody
+    }
+
 };
--- frontend/src/routes/booking/[slug]/+page.svelte
+++ frontend/src/routes/booking/[slug]/+page.svelte
@@ -4,22 +4,26 @@
 	let { data } = $props();
 
 	// some mock data to send as a prop, for testing.
-	let booking = {
-		Id: 'mock Id for booking',
-		StartDate: 'Add a date here',
-		EndDate: 'Add a date here as well'
-	};
+	// let booking = {
+	// 	Id: 'mock Id for booking',
+	// 	StartDate: 'Add a date here',
+	// 	EndDate: 'Add a date here as well'
+	// };
+
+	let booking = data.booking;
 
 	let user = {
 		Id: 'mock id for the user',
 		Name: 'Mock Mockson'
 	};
 
-	let room = {
-		Id: 'mock id for the room',
-		Number: 404,
-		Name: 'Made up'
-	};
+	// let room = {
+	// 	Id: 'mock id for the room',
+	// 	Number: 404,
+	// 	Name: 'Made up'
+	// };
+
+	let room = data.room;
 
 	let activity = {
 		Id: 'Mock id for the activity',
